79a33616eb7971c3d482d6f4a27b70a884f56cb2
umanggupta <168900707+umangg3010@users.noreply.github.com>
2024-07-02 22:00:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementsByClassName("icon")[0].addEventListener("click", () => {
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
